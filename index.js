26653192cf094341ad48f4181c7adf5d0f04bf3f
Stephen Williams <armando2002@gmail.com>
2018-11-20 11:12:47 -0800

    working demo

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /* Function to pick team based on random # generator */
+/* Set key value pair for each team in the array set to requested # of people in team, once array reaches 0, remove the team from the available selection */
+
+
 var teams = ['Ms-Pac Man: Team Blinky', 'Ms-Pac Man: Team Clyde', 'Donkey Kong: Team Barrel', 'Donkey Kong: Team Hammer', 'Space Invaders', 'Dig Dug'];
 
 
@@ -7,7 +10,7 @@ function pickTeam() {
     $('#pickform').on('submit', function(event){
         event.preventDefault();
         var randomTeam = teams[Math.floor(Math.random()*teams.length)];
-        console.log('Your team is '+randomTeam);
+        $('#team').html("Your team is "+randomTeam);
     });
 };
 
